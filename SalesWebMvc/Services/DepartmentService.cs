66530870e7333d33ded8072d085cdaeadb78fd70
Kodaac <nogueira.hs@hotmail.com>
2023-01-10 21:08:57 -0300

    SellerFormViewModel and Department select componente

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _contexto;
+
+        public DepartmentService(SalesWebMvcContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _contexto.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
